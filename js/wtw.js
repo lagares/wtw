94ed44428e58b9a4faa917e8e068c021f41d411a
Ariel <weebly@weebly-trialweek1-mbp.local>
2015-02-06 14:39:14 -0800

    angularize page content

--- js/wtw.js
+++ js/wtw.js
@@ -1,6 +1,6 @@
 
 // angularjs controller
-var miniWeebly = angular.module('miniWeebly', []);
+var miniWeebly = angular.module('miniWeebly', ['ngSanitize']);
 
 	// allows auto-select text on add/edit page input fields
 	miniWeebly.directive('selectContents', function () {
@@ -17,87 +17,103 @@ var miniWeebly = angular.module('miniWeebly', []);
 	miniWeebly.controller('pgCtrl', function ($scope){
 
 		var j; // index of oldName, set on editEnable and used by editPageName
+		// var enabled = false;
+		// var caution = false;
 
-		$scope.pages = ['PAGE']; // this presupposes the existence of at least one page, named PAGE
-		$scope.pageName = 'ADD NEW PAGE';
+		// page names and pages as objects
+		$scope.pages = [ 
+			{ 'name': 'PAGE ZERO' },
+			{ 'name': 'PAGE ONE' },
+			{ 'name': 'PAGE TWO' }
+		];
 
-		$scope.addPage = function() {
+		// page names and pages as array
+		// $scope.pages = [ 'PAGE ZERO', 'PAGE ONE', 'PAGE TWO' ];	
+
+		// $scope.page = 'ADD NEW PAGE'; 
+
+		// this simulates pre-existing/edited content
+		$scope.blocks = [
+			{ 'page': 'PAGE ZERO', 'type': 'title', 'contents': '<h1>ipsa quae ab illo</h1>' },
+			{ 'page': 'PAGE ZERO', 'type': 'image', 'contents': '<img src="http://lagares.github.io/wtw/images/Image-Placeholder.png" alt="image" />' },
+			{ 'page': 'PAGE ZERO', 'type': 'text', 'contents': '<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, </p>' },
+			{ 'page': 'PAGE ZERO', 'type': 'text', 'contents': '<p>Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>' },
+			{ 'page': 'PAGE ONE', 'type': 'title', 'contents': '<h1>Totam rem aperiam</h1>' },
+			{ 'page': 'PAGE ONE', 'type': 'text', 'contents': '<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>' },
+			{ 'page': 'PAGE ONE', 'type': 'text', 'contents': '<p>Fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.</p>' },
+			{ 'page': 'PAGE ONE', 'type': 'text', 'contents': '<p>Consectetur, adipisci velit, sed consequuntur magni dolores eos qui ratione et dolore magnam aliquam quaerat voluptatem eos qui ratione voluptatem sequi.</p>' },
+			{ 'page': 'PAGE TWO', 'type': 'title', 'contents': '<h1>sed quia consequuntur</h1>' },
+			{ 'page': 'PAGE TWO', 'type': 'image', 'contents': '<img src="http://lagares.github.io/wtw/images/Image-Placeholder.png" alt="image" />' },
+			{ 'page': 'PAGE TWO', 'type': 'image', 'contents': '<img src="http://lagares.github.io/wtw/images/Image-Placeholder.png" alt="image" />' },
+			{ 'page': 'PAGE TWO', 'type': 'image', 'contents': '<img src="http://lagares.github.io/wtw/images/Image-Placeholder.png" alt="image" />' } 
+		];
+
+		$scope.placeholderContent = [
+			{ 'text'	: '<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>'},
+			{ 'image'	: '' },
+			{ 'nav'		: '' },
+			{ 'title'	: '<h1>Add Title Here</h1>'}
+		];
+
+		$scope.currentPage = null;
+
+		$scope.setCurrentPage = function(page) {
+			$scope.currentPage = page;
+		};
+
+		$scope.isCurrentPage = function(page) {
+			return $scope.currentPage !== null && page.name === $scope.currentPage.name;
+		};
+
+		$scope.addPage = function(page) {
 			// create a new page object w corresponding child scope here
 			// ...
 
-			/* something like
-			{
-				pageName : 'PAGE',
-				placeholder1 : 'placeholder1content',
-				placeholder2 : 'placeholder2content'
-				...
-			}
-
-			or 
-
-			{
-				pageName : 'PAGE',
-				placholders : [ 'placeholder1', 'placeholder1content' ], [ 'placeholder2', 'placeholder2content', ... ]
- 				...
-			}
-
-			*/
-
-			// add the pageName here for demo purposes
-			$scope.pages.push($scope.pageName);
-			$scope.pageName = 'ADD NEW PAGE';
+			$scope.pages.push($scope.name);
+			$scope.page = 'ADD NEW PAGE';
 			$scope.addPageForm.$setPristine();
 		};
 
-		$scope.removePage = function(pageName) {
-			var i = $scope.pages.indexOf(pageName);
+		$scope.removePage = function(page) {
+			var i = $scope.pages.indexOf(page);
 			$scope.pages.splice(i, 1);
 		};
 
-		$scope.editEnable = function(pageName) {
+		$scope.editEnable = function(page) {
 			$scope.enabled = !$scope.enabled;
-			var oldName = pageName;
+			var oldName = page;
 			j = $scope.pages.indexOf(oldName);
 		};
 
-		$scope.editPageName = function(pageName) {
+		$scope.editPageName = function(page) {
 			$scope.enabled = !$scope.enabled;
-			$scope.pages.splice(j, 1, pageName);
+			$scope.pages.splice(j, 1, page);
 		};
 
-		// to deal with urls and pages
-		miniWeebly.config(function($routeProvider) {
-			$routeProvider.
-				when('/:pageName', {
-					templateUrl: 'index.html',
-					controller: 'miniWeebly'
-				}).
-				otherwise({
-				redirectTo: '/'
-			});
-		});
-
-		// on change of .tools-pages, #content-added-items, let Angular know so that newly-created items become available for changes such as edits, etc.
-		function update() {
-			$scope.$apply();
+		$scope.cautionOn = function(block){
+			$scope.caution = true;
 		};
+
+		$scope.cautionOff = function(block){
+		 	$scope.caution = false;
+		};
+
+		$scope.removeBlock = function(block) {
+			var i = $scope.blocks.indexOf(block);
+			$scope.blocks.splice(i, 1);			
+		}
 
 	/* TODO:
 
-	*- unable to edit the first pageName - why?
-	*- enable drag-drop
-	*- enable auto-fill screen-width
-	*- on click added-content-item, make editable
-
 	1- submit editable content onBlur 
 	2- enable half-size when drag on-top of another - "columns"
-	3- implement 'pages' - objects made up of name, an array of content frames' names?, the type of each content frame, the content of each content frame - Wahid says use individual Angular $scopes for each 'page', to keep their contents apart
 	3- BUG - on tool drag stop content is added to the pasteboard even if dropped on sidebar, why?
 	4- BUG - only content frames already on screen are deletable/editable. New ones do not delete, why?
-	5- BUG - only first page-badge accepts :hover delete color, why?
-	6- How to update the URL fragment when the page name is edited?
 	7- enable resize + handles
-	8- enable 'selected' state on page-badge and pasteboard button nav
+	*- enable 'selected' state on page-badge and pasteboard button nav
+	*- BUG - only first page-badge accepts :hover delete color, why?
+	*- How to update the URL fragment when the page name is edited?
+	*- implement 'pages' - objects made up of name, an array of content frames' names?, the type of each content frame, the content of each content frame - Wahid says use individual Angular $scopes for each 'page', to keep their contents apart
 
 	*/
 	});
@@ -137,8 +153,8 @@ $(function() {
 
 	$('.placeholder').resizable({
 		// autoHide: true,
-		containment: 'parent',
 		// ghost: true,
+		containment: 'parent',
 		handles: 'e, s, w'
 	});
 
@@ -164,6 +180,7 @@ $(function() {
 			.append('<span class="delete-element"></span>')
 			.append( dummyContent )
 			.addClass('placeholder ' + contentType + '-placeholder')
+			// .addClass('placeholder ' + {{block.type}}) no longer depends on passing contentType via JS outside NG
 			.appendTo($('#content-added-items'));
 	};
 
@@ -179,18 +196,14 @@ $(function() {
 	// make content editable - text content type only for now
 	$('.text-placeholder p').click( function() {
 		var content = $(this).text();
-		$(this).replaceWith('<form id="textEditor"><textarea>' + content + '</textarea></form>');
+		$(this).replaceWith('<form id="textEditor"><textarea tabindex="-1">' + content + '</textarea></form>');
 	});
 
 	// moving out of the placeholder signifies finished editing, remove the textarea and replace with a <p>
-	$('.placeholder').focus( function() {
-		// var editedContent = $(this).text(); // store the newly-edited text
-		// $(this).replaceWith('<span class="delete-element"></span><p>' + editedContent + '</p>');
+	$('.placeholder').on('focusout', function() {
+		var editedContent = $(this).text(); // store the newly-edited text
+		$(this).empty()
+		.append('<span class="delete-element"></span><p>' + editedContent + '</p>');
 	});
-
-	$('.pagesNav .button').click( function() {
-		$('.pagesNav .button').removeClass('selected');
-		$(this).addClass('selected');
-	})
 
 });
